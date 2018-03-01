0726172a4b3006b0fedb686866e8850de80cb3d9
gordon <1029052540@qq.com>
2018-03-02 02:31:28 +0800

    add functions and tests

--- go/tour/basics.go
+++ go/tour/basics.go
@@ -13,3 +13,17 @@ func Packages() {
 func Import() {
 	fmt.Printf("Now you have %g problems\n", math.Sqrt(7))
 }
+
+func Add(x int, y int) int {
+	return x + y
+}
+
+func ForSum(x int) int {
+	sum := 0
+	fmt.Printf("the result of sum from 1 to %d is ", x)
+	for i := 1; i <= x; i++ {
+		sum = sum + i
+	}
+	fmt.Println(sum)
+	return sum
+}
--- go/tour/basics_test.go
+++ go/tour/basics_test.go
@@ -1,6 +1,10 @@
 package tour
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+	"math/rand"
+)
 
 func TestPackages(t *testing.T) {
 	Packages()
@@ -15,3 +19,17 @@ func BenchmarkPackages(b *testing.B) {
 func TestImport(t *testing.T) {
 	Import()
 }
+
+func TestAdd(t *testing.T) {
+	a := rand.Int()
+	b := rand.Int()
+	c := Add(a, b)
+	fmt.Printf("%d + %d = %d", a, b, c)
+}
+
+func TestForSum(t *testing.T) {
+	x := 10000
+	ForSum(x)
+}
+
+
