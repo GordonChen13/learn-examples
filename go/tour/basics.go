4a5b918ad24568f316f07f18a2c14bd74e908e8b
gordon <1029052540@qq.com>
2018-03-08 23:09:50 +0800

    add funcs and tests

--- go/tour/basics.go
+++ go/tour/basics.go
@@ -85,3 +85,14 @@ func FibonacciClo() func() int {
 		return next
 	}
 }
+
+type IPAddr [4]byte
+func Stringer()  {
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+}
