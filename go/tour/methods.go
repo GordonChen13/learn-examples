0af7a41aeb2d81b718831e04e36e6c2c6828dd4d
gordon <1029052540@qq.com>
2018-03-09 23:45:09 +0800

    add funcs and tests

--- go/tour/methods.go
+++ go/tour/methods.go
@@ -1,1 +1,14 @@
 package tour
+
+import "fmt"
+
+type IPAddr [4]byte
+func Stringer()  {
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+}
