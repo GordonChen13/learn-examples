527919f7b08fa430dd0db88dbee600ef5809c9f8
gordon <1029052540@qq.com>
2018-03-11 23:24:36 +0800

    add funcs and tests

--- go/tour/methods.go
+++ go/tour/methods.go
@@ -3,12 +3,23 @@ package tour
 import "fmt"
 
 type IPAddr [4]byte
-func Stringer()  {
-	hosts := map[string]IPAddr{
-		"loopback":  {127, 0, 0, 1},
-		"googleDNS": {8, 8, 8, 8},
+func (ip IPAddr) String() string {
+	return fmt.Sprintf("%v.%v.%v.%v", ip[0], ip[1], ip[2], ip[3])
+}
+
+type SqrtError float64
+
+func (e SqrtError) Error() string  {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
+func LoopSqrtError(s SqrtError, n int) (float64, e error)  {
+	if s < 0 {
+		e := "error"
 	}
-	for name, ip := range hosts {
-		fmt.Printf("%v: %v\n", name, ip)
+	var z SqrtError
+	z = 1.0
+	for i := 0; i < n; i ++ {
+		z -= (s * s - z) / 2*z
 	}
 }
---
+++ go/tour/methods_test.go
@@ -0,0 +1,16 @@
+package tour
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestIPAddr_String(t *testing.T) {
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+}
