08dfeb7fc5fe2fd91d9c7e42c6f8db438bd8b222
gordon <1029052540@qq.com>
2018-03-15 23:55:44 +0800

    add funcs and tests

--- go/tour/methods.go
+++ go/tour/methods.go
@@ -3,6 +3,8 @@ package tour
 import (
 	"fmt"
 	"io"
+	"image"
+	"image/color"
 )
 
 type IPAddr [4]byte
@@ -46,4 +48,25 @@ func (rot *rot13Reader) Read(b []byte) (int, error) {
 		}
 	}
 	return n, e
+}
+
+type Image struct {
+	w, h int
+	c    uint8
+}
+
+func (img *Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, img.w, img.h)
+}
+
+func (img *Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (img *Image) At(x, y int) color.Color {
+	return color.RGBA{img.c + uint8(x*y+x*y), img.c + uint8(x*y+x*y), 255, 255}
+}
+func ColorImage() {
+	m := Image{100, 100, 128}
+	pic.ShowImage(&m)
 }
