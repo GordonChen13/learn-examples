fa599faa21c634280901a04d593f050c52443405
GordonChen13 <1029052540@qq.com>
2018-04-26 18:41:20 +0800

    <feat>:add head first java ch2 code

---
+++ java/HeadFirstJava/ch2/GuessGame.java
@@ -0,0 +1,11 @@
+public class GuessGame {
+    Player p1;
+    Player p2;
+    Player p3;
+
+    public void startGame() {
+        p1 = new Player();
+        p2 = new Player();
+        p3 = new Player();
+    }
+}
