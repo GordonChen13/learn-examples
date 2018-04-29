f1235cd0fc573630b268acf49b1388f494ac6963
GordonChen13 <1029052540@qq.com>
2018-04-29 23:38:24 +0800

    <feat>:add test

--- java/HeadFirstJava/ch2/DogTestDrive.java
+++ java/HeadFirstJava/ch2/DogTestDrive.java
@@ -2,7 +2,14 @@ class DogTestDrive {
     public static void main(String[] args) {
 
         Dog d = new Dog();
+//        Dog d2 = new Dog();
+        Dog d2 = d;
+
         d.size = 40;
         d.bark();
+
+        d2.size = 50;
+
+        System.out.println("new size of d is: " + d.size);
     }
 }
