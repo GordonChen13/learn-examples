bae91821de40fa9acc5cd13319db1940750f847b
GordonChen13 <1029052540@qq.com>
2018-05-10 00:57:14 +0800

    <feat>:add head first java example code

--- java/HeadFirstJava/ch6/DotCom.java
+++ java/HeadFirstJava/ch6/DotCom.java
@@ -1,5 +1,32 @@
+import java.util.*;
+
 public class DotCom {
     private String name;
+    private ArrayList<String> locationCells;
 
-    public void __con
+    public void setLocationCells(ArrayList<String> cells) {
+        locationCells = cells;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String checkYourself(String userGuess) {
+        String result = "miss";
+        int index = locationCells.indexOf(userGuess);
+
+        if (index >= 0) {
+            locationCells.remove(userGuess);
+
+            if (locationCells.isEmpty()) {
+                result = "kill";
+                System.out.println("Ouch! You sunk " + name + " : ( ");
+            } else {
+                result = "hit";
+            }
+        }
+
+        return result;
+    }
 }
