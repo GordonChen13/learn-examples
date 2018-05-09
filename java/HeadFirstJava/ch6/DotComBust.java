4d300a7582d1ef51baa7c2776ba89f9ffb234b53
GordonChen13 <1029052540@qq.com>
2018-05-09 23:59:37 +0800

    add heard firsh java code

--- java/HeadFirstJava/ch6/DotComBust.java
+++ java/HeadFirstJava/ch6/DotComBust.java
@@ -26,4 +26,46 @@ public class DotComBust {
             dotComToSet.setLocationCells(newLocation);
         }
     }
+
+    private void startPlaying() {
+        while (!dotComList.isEmpty()) {
+            String userGuess = helper.getUserInput("Enter a guess");
+            checkUserGuess(userGuess);
+        }
+        finishGame();
+    }
+
+    private void checkUserGuess(String userGuess) {
+        numOfGuess++;
+        String result = "miss";
+
+        for (DotCom dotComToTest : dotComLists) {
+            result = dotComToTest.checkYourself(userGuess);
+            if (result.equals("hit")) {
+                break;
+            }
+            if (result.equals("kill")) {
+                dotComLists.remove(dotComToTest);
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+
+    private void finishGame() {
+        System.out.println("All dot coms are dead! Your stock is now worthless.");
+        if (numOfGuess <= 18) {
+            System.out.println("It only took you " + numOfGuess + "guesses.");
+            System.out.println("You got out before your opotions sank.");
+        } else {
+            System.out.println("Took you long enough. " + numOfGuess + "guesses.");
+            System.out.println("Fish are dance with your options");
+        }
+    }
+
+    public static void main(String[] args) {
+        DotComBust game = new DotComBust();
+        game.setUpGame();
+        game.startPlaying();
+    }
 }
