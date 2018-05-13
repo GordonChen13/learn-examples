1d2bafb7d9630067e1a4224dd891d80d9e4c995f
GordonChen13 <1029052540@qq.com>
2018-05-14 05:34:49 +0800

    <feat>:add heard first java learn example code

--- java/HeadFirstJava/ch8/MakeCanine.java
+++ java/HeadFirstJava/ch8/MakeCanine.java
@@ -2,6 +2,8 @@ public class MakeCanine {
     public void go() {
         Canine c;
         c = new Dog();
+
+        // class Canine is marked abstract, so the compiler will NOT let you do this.
         c = new Canine();
         c.roam();
     }
