f0b962d4e0028d47441f27b2af69cdbe1e61c8e3
GordonChen13 <1029052540@qq.com>
2018-05-15 18:29:48 +0800

    <feat>:add head first java learn example code

--- java/HeadFirstJava/ch9/Duck.java
+++ java/HeadFirstJava/ch9/Duck.java
@@ -1,5 +1,17 @@
 public class Duck {
+    int size;
+
     public Duck() {
+        size = 20;
+
+        System.out.println("the default size is " + size);
+    }
+
+    public Duck(int duckSize) {
         System.out.println("Quack");
+
+        size = duckSize;
+
+        System.out.println("size is " + size);
     }
 }
--- java/HeadFirstJava/ch9/UseADuck.java
+++ java/HeadFirstJava/ch9/UseADuck.java
@@ -1,5 +1,7 @@
 public class UseADuck {
     public static void main (String[] args) {
-        Duck d = new Duck();
+        Duck d = new Duck(42);
+
+        Duck d2 = new Duck();
     }
 }
