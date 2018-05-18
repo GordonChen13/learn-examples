3781501dd404acdd463319c36ba6ef12697868c4
GordonChen13 <1029052540@qq.com>
2018-05-18 18:26:01 +0800

    <feat>:add head first java learning example code

---
+++ java/HeadFirstJava/MusicPlayer/MusicTest1.java
@@ -0,0 +1,18 @@
+import javax.sound.midi.*;
+
+public class MusicTest1 {
+    public void play() {
+        try {
+            Sequencer sequencer = MidiSystem.getSequencer();
+            System.out.println("We got a sequencer");
+        } catch (MidiUnavailableException ex) {
+            System.out.println("Bummer");
+        }
+    }
+
+    public static void main(String[] args) {
+        MusicTest1 mt = new MusicTest1();
+
+        mt.play();
+    }
+}
