3d4b1f36d32e285a49ff7173ad3138cc0b4383bf
GordonChen13 <1029052540@qq.com>
2018-05-25 16:12:00 +0800

    <feat>:add head first java learning example code

--- java/HeadFirstJava/ch12/MyDrawPanel.java
+++ java/HeadFirstJava/ch12/MyDrawPanel.java
@@ -4,7 +4,21 @@ import javax.swing.*;
 public class MyDrawPanel extends JPanel {
 
     public void paintComponent(Graphics g) {
-        g.setColor(Color.orange);
-        g.fillRect(20, 50, 100, 100);
+        Graphics2D g2d = (Graphics2D) g;
+
+        int red = (int) (Math.random() * 255);
+        int green = (int) (Math.random() * 255);
+        int blue = (int) (Math.random() * 255);
+        Color startColor = new Color(red, green, blue);
+
+        int red2 = (int) (Math.random() * 255);
+        int green2 = (int) (Math.random() * 255);
+        int blue2 = (int) (Math.random() * 255);
+        Color endColor= new Color(red2, green2, blue2);
+
+        GradientPaint gradient = new GradientPaint(70, 70, startColor, 150, 150, endColor);
+        g2d.setPaint(gradient);
+        g2d.fillOval(70, 70, 100, 100);
+
     }
 }
