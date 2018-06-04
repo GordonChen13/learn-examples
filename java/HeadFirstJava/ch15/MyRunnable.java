8d7c033418fe0d8549107ca752c07ff139ec917b
GordonChen13 <1029052540@qq.com>
2018-06-04 18:32:07 +0800

    <feat>: add head first java learning example code

---
+++ java/HeadFirstJava/ch15/MyRunnable.java
@@ -0,0 +1,13 @@
+public class MyRunnable implements Runnable {
+    public void run() {
+        go();
+    }
+
+    public void go() {
+        doMore();
+    }
+
+    public void doMore() {
+        System.out.println("top o' the stack");
+    }
+}
---
+++ java/HeadFirstJava/ch15/ThreadTester.java
@@ -0,0 +1,9 @@
+class ThreadTester {
+    public static void main(String[] args) {
+        Runnable threadJob = new MyRunnable();
+        Thread myThread = new Thread(threadJob);
+        myThread.start();
+
+        System.out.println("back in main");
+    }
+}
