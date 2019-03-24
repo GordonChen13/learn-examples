9f67d6b768932083c03313e71fa09fece260dd88
Gordon <1029052540@qq.com>
2019-03-24 23:45:20 +0800

    <feat>: add go reflection test example

---
+++ go/learnGoWithTests/reflection/v2/walk_test.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+type Person struct {
+	Name string
+	Profile Profile
+}
+type Profile struct {
+	Age int
+	City string
+}
+
+func TestWalk(t *testing.T) {
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with two string field",
+			struct {
+				Name string
+				City string
+			}{"Chris", "ShenZhen"},
+			[]string{"Chris", "ShenZhen"},
+		},
+		{
+			"Struct with non string field",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+		{
+			"Nested fields",
+			Person{"Chris", Profile{33, "ShenZhen"}},
+			[]string{"Chris", "ShenZhen"},
+		},
+		{
+			"Pointer to things",
+			&Person{"Chris", Profile{33, "ShenZhen"}},
+			[]string{"Chris", "ShenZhen"},
+		},
+		{
+			"Slices",
+			&Person{"Chris", Profile{33, "ShenZhen"}},
+			[]string{"Chris", "ShenZhen"},
+		},
+	}
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
