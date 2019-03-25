7dec3f74941111e7dfe8e526ab6bbeca137c1c7e
GordonChen13 <1029052540@qq.com>
2019-03-25 14:38:23 +0800

    <feat>: add go sync test example

---
+++ go/learnGoWithTests/sync/v2/counter_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestCounter(t *testing.T) {
+	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
+		counter := &Counter{}
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+
+		assertCounter(t, counter, 3)
+	})
+	t.Run("it runs safely councurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := &Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func(w *sync.WaitGroup) {
+				counter.Inc()
+				w.Done()
+			}(&wg)
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
+	})
+}
+
+func assertCounter(t *testing.T, got *Counter, want int)  {
+	t.Helper()
+	if got.Value() != want {
+		t.Errorf("got %d, want %d", got.Value(), want)
+	}
+}
