5969dfe31a2d878304f5727dfb3eecbe80c20f35
Gordon <1029052540@qq.com>
2019-04-04 23:47:17 +0800

    <feat>: add go io test example

---
+++ go/learnGoWithTests/io/v2/FileSystemStore.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+)
+
+type FileSystemStore struct {
+	database io.ReadWriteSeeker
+	league League
+}
+
+func NewFileSystemStore(database io.ReadWriteSeeker) *FileSystemStore {
+	database.Seek(0,0)
+	league, _ := NewLeague(database)
+	return &FileSystemStore{
+		database:database,
+		league:league,
+	}
+}
+
+func (f *FileSystemStore) GetPlayerScore(name string) int {
+	player := f.league.Find(name)
+
+	if player != nil {
+		return player.Wins
+	}
+
+	return 0
+}
+
+func (f *FileSystemStore) RecordWin(name string)  {
+	player := f.league.Find(name)
+
+	if player != nil {
+		player.Wins++
+	} else {
+		f.league = append(f.league, Player{name, 1})
+	}
+
+	f.database.Seek(0,0)
+	json.NewEncoder(f.database).Encode(f.league)
+	return
+}
+
+func (f *FileSystemStore) GetLeague() League {
+	return f.league
+}
---
+++ go/learnGoWithTests/io/v2/league.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+)
+
+type League []Player
+
+func NewLeague(rdr io.Reader) ([]Player, error) {
+	var league []Player
+	err := json.NewDecoder(rdr).Decode(&league)
+	if err != nil {
+		err = fmt.Errorf("problem parsing league, %v", err)
+	}
+
+	return league, err
+}
+
+func (l League) Find(name string) *Player {
+	for i,p := range l {
+		if p.Name == name {
+			return &l[i]
+		}
+	}
+	return nil
+}
