147e525fd7f6eafb4049810cebb17ac970258c5a
Gordon <1029052540@qq.com>
2019-05-17 01:22:37 +0800

    <feat>: add gopl example

--- go/gopl/ch1/1.3.go
+++ go/gopl/ch1/1.3.go
@@ -13,7 +13,6 @@ func main()  {
 	repeat := os.Args[1]
 	count, err := strconv.Atoi(os.Args[2])
 	sa := make([]string, count)
-	var s string
 
 	if err != nil {
 		log.Fatal("args 2 must be number")
@@ -22,16 +21,17 @@ func main()  {
 		sa = append(sa, repeat)
 	}
 
+    s1 := ""
 	before := time.Now().Nanosecond()
 	for _, str := range sa {
-		s = str + " "
+		s1 += str + " "
 	}
 	consume1 := time.Now().Nanosecond() - before
 	fmt.Println(consume1)
 
-	s = ""
+	s2 := ""
 	before2 := time.Now().Nanosecond()
-	s = strings.Join(sa, " ")
+	s2 += strings.Join(sa, " ")
 	consume2 := time.Now().Nanosecond() - before2
 	fmt.Println(consume2)
 }
