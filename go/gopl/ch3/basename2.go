0e10cc38827ed2f7b913668a6c33668722a32779
Gordon <1029052540@qq.com>
2019-06-01 23:58:17 +0800

    <feat>: add gopl example

---
+++ go/gopl/ch3/basename2.go
@@ -0,0 +1,13 @@
+package main
+
+import "strings"
+
+func basename2(s string) string {
+	slash := strings.LastIndex(s, "/")
+	s = s[slash+1:]
+	if dot := strings.LastIndex(s, "."); dot >= 0 {
+		s = s[:dot]
+	}
+
+	return s
+}
