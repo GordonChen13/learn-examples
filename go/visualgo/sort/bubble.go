8785028fb990f9a31680de92d2cfee4fce2faa7c
GordonChen13 <1029052540@qq.com>
2019-06-05 19:15:23 +0800

    <feat> add visualgo bubble sort and test

---
+++ go/visualgo/sort/bubble.go
@@ -0,0 +1,14 @@
+package sort
+
+func Bubble(nums []int) []int {
+	for i := range nums {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i] > nums[j] {
+				nums[i], nums[j] = nums[j], nums[i]
+			}
+		}
+	}
+	return nums
+}
+
+
--- go/visualgo/sort/sort_test.go
+++ go/visualgo/sort/sort_test.go
@@ -1,4 +1,24 @@
 package sort
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
+func TestBubble(t *testing.T) {
+	cases := [] struct{
+		name string
+		input []int
+		want []int
+	}{
+		{"small int", []int{1, 8, 10, 7, 3, 88, 56, 93}, []int{1, 3, 7, 8, 10, 56, 88, 93}},
+		{"big int", []int{10000, 8, 10, 7000, 3, 88, 5600000, 93}, []int{3, 8, 10, 88, 93, 7000, 10000, 5600000}},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			get := Bubble(c.input)
+			assert.Equal(t, c.want, get)
+		})
+	}
+}
