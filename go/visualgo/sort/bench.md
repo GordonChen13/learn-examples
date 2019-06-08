4df80a434ae8a7100d255fc9f3ea9c0aee2c1dea
Gordon <1029052540@qq.com>
2019-06-08 23:51:45 +0800

    <feat>: add go sort benchmark

---
+++ go/visualgo/sort/bench.md
@@ -0,0 +1,25 @@
+goos: darwin
+goarch: amd64
+pkg: github.com/GordonChen13/learn-examples/go/visualgo/sort
+BenchmarkBubble10-12        	20000000	       104 ns/op
+BenchmarkBubble100-12       	  200000	     10101 ns/op
+BenchmarkBubble1000-12      	    2000	    922474 ns/op
+BenchmarkBubble10000-12     	      20	  91744542 ns/op
+BenchmarkBubble100000-12    	       1	17931528408 ns/op
+BenchmarkSelect10-12        	10000000	       175 ns/op
+BenchmarkSelect100-12       	  100000	     19664 ns/op
+BenchmarkSelect1000-12      	    1000	   1860181 ns/op
+BenchmarkSelect10000-12     	      10	 185290561 ns/op
+BenchmarkSelect100000-12    	       1	18851947603 ns/op
+BenchmarkInsert10-12        	100000000	        11.4 ns/op
+BenchmarkInsert100-12       	20000000	       104 ns/op
+BenchmarkInsert1000-12      	 2000000	       930 ns/op
+BenchmarkInsert10000-12     	  200000	      9170 ns/op
+BenchmarkInsert100000-12    	   20000	     91972 ns/op
+BenchmarkMerge10-12         	 3000000	       551 ns/op
+BenchmarkMerge100-12        	  200000	      7297 ns/op
+BenchmarkMerge1000-12       	   20000	     85898 ns/op
+BenchmarkMerge10000-12      	    2000	    990679 ns/op
+BenchmarkMerge100000-12     	     100	  11082875 ns/op
+PASS
+ok  	github.com/GordonChen13/learn-examples/go/visualgo/sort	74.590s
--- go/visualgo/sort/sort_test.go
+++ go/visualgo/sort/sort_test.go
@@ -2,11 +2,24 @@ package sort
 
 import (
 	"github.com/stretchr/testify/assert"
+	"math/rand"
 	"testing"
 )
 
-func TestBubble(t *testing.T) {
-	cases := [] struct{
+var cases []struct{
+	name string
+	input []int
+	want []int
+}
+
+var int10 []int
+var int100 []int
+var int1000 []int
+var int10000 []int
+var int100000 []int
+
+func init() {
+	cases = [] struct{
 		name string
 		input []int
 		want []int
@@ -15,6 +28,23 @@ func TestBubble(t *testing.T) {
 		{"big int", []int{10000, 8, 10, 7000, 3, 88, 5600000, 93}, []int{3, 8, 10, 88, 93, 7000, 10000, 5600000}},
 	}
 
+	int10 = intGen(10)
+	int100 = intGen(100)
+	int1000 = intGen(1000)
+	int10000 = intGen(10000)
+	int100000 = intGen(100000)
+}
+
+func intGen(num int) []int {
+	res := make([]int, num)
+	for i := 0; i < num; i++ {
+		res = append(res, rand.Int())
+	}
+
+	return res
+}
+
+func TestBubble(t *testing.T) {
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
 			get := Bubble(c.input)
@@ -23,16 +53,38 @@ func TestBubble(t *testing.T) {
 	}
 }
 
-func TestSelect(t *testing.T) {
-	cases := [] struct{
-		name string
-		input []int
-		want []int
-	}{
-		{"small int", []int{1, 8, 10, 7, 3, 88, 56, 93}, []int{1, 3, 7, 8, 10, 56, 88, 93}},
-		{"big int", []int{10000, 8, 10, 7000, 3, 88, 5600000, 93}, []int{3, 8, 10, 88, 93, 7000, 10000, 5600000}},
+func BenchmarkBubble10(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Bubble(int10)
 	}
+}
 
+func BenchmarkBubble100(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Bubble(int100)
+	}
+}
+
+func BenchmarkBubble1000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Bubble(int1000)
+	}
+}
+
+func BenchmarkBubble10000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Bubble(int10000)
+	}
+}
+
+func BenchmarkBubble100000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Bubble(int100000)
+	}
+}
+
+
+func TestSelect(t *testing.T) {
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
 			get := Select(c.input)
@@ -41,16 +93,37 @@ func TestSelect(t *testing.T) {
 	}
 }
 
-func TestInsert(t *testing.T) {
-	cases := [] struct{
-		name string
-		input []int
-		want []int
-	}{
-		{"small int", []int{1, 8, 10, 7, 3, 88, 56, 93}, []int{1, 3, 7, 8, 10, 56, 88, 93}},
-		{"big int", []int{10000, 8, 10, 7000, 3, 88, 5600000, 93}, []int{3, 8, 10, 88, 93, 7000, 10000, 5600000}},
+func BenchmarkSelect10(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Select(int10)
 	}
+}
 
+func BenchmarkSelect100(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Select(int100)
+	}
+}
+
+func BenchmarkSelect1000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Select(int1000)
+	}
+}
+
+func BenchmarkSelect10000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Select(int10000)
+	}
+}
+
+func BenchmarkSelect100000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Select(int100000)
+	}
+}
+
+func TestInsert(t *testing.T) {
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
 			get := Insert(c.input)
@@ -59,20 +132,71 @@ func TestInsert(t *testing.T) {
 	}
 }
 
-func TestMerge(t *testing.T) {
-	cases := [] struct{
-		name string
-		input []int
-		want []int
-	}{
-		{"small int", []int{1, 8, 10, 7, 3, 88, 56, 93}, []int{1, 3, 7, 8, 10, 56, 88, 93}},
-		{"big int", []int{10000, 8, 10, 7000, 3, 88, 5600000, 93}, []int{3, 8, 10, 88, 93, 7000, 10000, 5600000}},
+func BenchmarkInsert10(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Insert(int10)
 	}
+}
 
+func BenchmarkInsert100(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Insert(int100)
+	}
+}
+
+func BenchmarkInsert1000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Insert(int1000)
+	}
+}
+
+func BenchmarkInsert10000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Insert(int10000)
+	}
+}
+
+func BenchmarkInsert100000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Insert(int100000)
+	}
+}
+
+func TestMerge(t *testing.T) {
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
 			get := MergeSort(c.input)
 			assert.Equal(t, c.want, get)
 		})
+	}
+}
+
+func BenchmarkMerge10(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		MergeSort(int10)
+	}
+}
+
+func BenchmarkMerge100(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		MergeSort(int100)
+	}
+}
+
+func BenchmarkMerge1000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		MergeSort(int1000)
+	}
+}
+
+func BenchmarkMerge10000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		MergeSort(int10000)
+	}
+}
+
+func BenchmarkMerge100000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		MergeSort(int100000)
 	}
 }
