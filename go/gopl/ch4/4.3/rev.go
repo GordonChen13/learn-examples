6a51f0d0ef4c086baac6d05541dd0bf93a85eaba
Gordon <1029052540@qq.com>
2019-06-15 23:48:45 +0800

    <feat> add gopl exercises

--- go/gopl/ch4/4.3/rev.go
+++ go/gopl/ch4/4.3/rev.go
@@ -1,9 +1,15 @@
 package main
 
-func reverse(s []int) []int {
+import "fmt"
+
+func main()  {
+	a := [...]int{0,1,2,3,4,5}
+	reverse(&a)
+	fmt.Println(a)
+}
+
+func reverse(s *[6]int) {
 	for i,j := 0, len(s)-1; i < j; i,j = i+1, j-1 {
 		s[i], s[j] = s[j], s[i]
 	}
-
-	return s
 }
---
+++ go/gopl/ch4/4.4/4.4.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func rotateLeft(a []int, numRot int) []int {
+	lenA := len(a)
+	if numRot <= 0 || numRot >= lenA {
+		return a
+	}
+	temp := make([]int, lenA)
+	for i,j := 0, numRot; i < lenA; i, j = i+1, j+1 {
+		if j == len(a) {
+			j = 0
+		}
+		temp[i] = a[j]
+	}
+
+	return temp
+}
+
+func main() {
+	s := []int{1,2,4,5}
+	fmt.Println(rotateLeft(s, 1))
+}
