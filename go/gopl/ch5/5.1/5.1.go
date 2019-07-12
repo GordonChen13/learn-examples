d43f837d31058a6f73320a652632c1088f7c59fb
Gordon <1029052540@qq.com>
2019-07-12 23:20:28 +0800

    <feat>: add gopl examples

---
+++ go/gopl/ch5/5.1/5.1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"os"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	visit(doc)
+}
+
+func visit(n *html.Node) {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				fmt.Println(a.Val)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		visit(c)
+	}
+}
