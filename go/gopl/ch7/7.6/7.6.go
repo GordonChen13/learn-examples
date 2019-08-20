dff907afe3e4cdd3fd796e1b05e105dbe0767fa2
Gordon <1029052540@qq.com>
2019-08-20 23:50:55 +0800

    <feat>: add gopl examples

---
+++ go/gopl/ch7/7.6/7.6.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+type Celsius float64
+type Fahrenheit float64
+type kelvin float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+	ZeroK         kelvin  = -273.15
+)
+
+func CToF(c Celsius) Fahrenheit {
+	return Fahrenheit(c*9.0/5.0 + 32.0)
+}
+
+func FToC(f Fahrenheit) Celsius {
+	return Celsius((f - 32.0) * 5.0 / 9.0)
+}
+
+func KToC(k kelvin) Celsius {
+	return Celsius(k/1000.0 - kelvin(float64(AbsoluteZeroC)))
+}
+
+func (c Celsius) String() string {
+	return fmt.Sprintf("%g°C", c)
+}
+
+type celsiusFlag struct {
+	Celsius
+}
+
+func (f *celsiusFlag) Set(s string) error {
+	var unit string
+	var value float64
+	fmt.Sscanf(s, "%f%s", &value, &unit)
+	switch unit {
+	case "C", "°C":
+		f.Celsius = Celsius(value)
+		return nil
+	case "F", "°F":
+		f.Celsius = FToC(Fahrenheit(value))
+		return nil
+	case "K":
+		f.Celsius = KToC(kelvin(value))
+		return nil
+	}
+	return fmt.Errorf("invalid temperature %q", s)
+}
+
+func CelsiusFlag(name string, value Celsius, usage string) *Celsius {
+	f := celsiusFlag{value}
+	flag.CommandLine.Var(&f, name, usage)
+	return &f.Celsius
+}
+
+var temp = CelsiusFlag("temp", 20.0, "the temperature")
+
+func main() {
+	flag.Parse()
+	fmt.Println(*temp)
+}
