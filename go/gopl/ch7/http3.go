4273bca7cec8bc2628a674911b4bbed3de1eb4bc
GordonChen13 <1029052540@qq.com>
2019-09-05 10:33:39 +0800

    <feat>: add gopl examples

---
+++ go/gopl/ch7/http3.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	db := database{"shoes": 50, "socks": 5}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/list", db.list)
+	mux.HandleFunc("/price", db.price)
+	log.Fatal(http.ListenAndServe("localhost:8000", mux))
+}
+
+type dollars float32
+
+func (d dollars) String() string {
+	return fmt.Sprintf("$%.2f", d)
+}
+
+type database map[string]dollars
+
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s \n", item, price)
+	}
+}
+
+func (db database) price(w http.ResponseWriter, req *http.Request)  {
+	item := req.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "no such item: %s \n", item)
+	}
+	fmt.Fprintf(w, "%s: %s \n", item, price)
+
+}
+
