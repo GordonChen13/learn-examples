22bd0e16ee56af61eafe7c142cf07c79601dc678
gordon <1029052540@qq.com>
2019-09-17 23:47:45 +0800

    <feat>: add gopl examples

--- go/gopl/ch7/eval/ast.go
+++ go/gopl/ch7/eval/ast.go
@@ -1,4 +1,14 @@
-package main
+package eval
+
+// An Expr is an arithmetic expression.
+type Expr interface {
+	// Eval returns the value of this Expr in the environment env.
+	Eval(env Env) float64
+	// Check reports errors in this Expr and adds its Vars to the set.
+	Check(vars map[Var]bool) error
+}
+
+//!+ast
 
 // A Var identifies a variable, e.g., x.
 type Var string
