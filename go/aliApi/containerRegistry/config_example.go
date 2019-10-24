7108d98f5ac5ca7d57c38debfa93b7ef3c426726
GordonChen13 <1029052540@qq.com>
2019-10-24 11:08:13 +0800

    <feat>: add aliyun api example

---
+++ go/aliApi/containerRegistry/config_example.go
@@ -0,0 +1,8 @@
+package main
+
+// Access Key ID用于标识访问者的身份
+//const ACCESSKEYID = "access key id"
+// Access Key Secret是用于加密签名字符串和服务器端验证签名字符串的密钥
+//const ACCESSKEYSECRET = "access key secret"
+// 地域
+//const REGIONID = "cn-shenzhen"
