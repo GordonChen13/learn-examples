0a8f3acbf3b83b04b0063c29571fc1eed6500d21
gordon <1029052540@qq.com>
2020-07-29 01:45:56 +0800

    feat: add go examples

--- go/gopl/ch8/8.2/ftp_server.go
+++ go/gopl/ch8/8.2/ftp_server.go
@@ -3,8 +3,11 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"log"
 	"net"
+	"os/exec"
+	"strings"
 )
 
 func main() {
@@ -29,7 +32,28 @@ func handleConn(c net.Conn) {
 	for {
 		scanner := bufio.NewScanner(c)
 		for scanner.Scan() {
-			c.Write([]byte(scanner.Text()))
+			input := strings.TrimSpace(scanner.Text())
+			switch {
+			case input == "close":
+				c.Close()
+			case strings.HasPrefix(input, "get"):
+				args := strings.Split(input, " ")
+				if len(args) < 2 {
+					c.Write([]byte("please add a filename"))
+				}
+				for _, fileName := range args[1:] {
+					c.Write([]byte(fmt.Sprintf("download file: %s", fileName)))
+				}
+
+			default:
+				args := strings.Split(input, " ")
+				cmd := exec.Command(args[0], args[1:]...)
+				output, err := cmd.CombinedOutput()
+				if err != nil {
+					c.Write([]byte(err.Error() + "\n"))
+				}
+				c.Write(output)
+			}
 		}
 	}
 }
---
+++ go/gopl/ch8/reverb1/reverb1.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+)
+
+func main() {
+	port := flag.String("port", "8000", "listen port")
+	flag.Parse()
+	listener, err := net.Listen("tcp", "localhost:"+*port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		echo(c, input.Text(), 1*time.Second)
+	}
+	c.Close()
+}
