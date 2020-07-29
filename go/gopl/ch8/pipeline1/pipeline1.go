71b572a04f98450c6dcab695b410a615f34513e5
gordon <1029052540@qq.com>
2020-07-30 01:07:43 +0800

    feat: add go examples

---
+++ go/gopl/ch8/pipeline1/pipeline1.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	// Counter
+	go func() {
+		for x := 0; ; x++ {
+			naturals <- x
+		}
+	}()
+
+	// Squarer
+	go func() {
+		for {
+			x, ok := <-naturals
+			if !ok {
+				break
+			}
+			squares <- x * x
+		}
+		close(squares)
+	}()
+
+	// Printer
+	for {
+		fmt.Println(<-squares)
+	}
+}
