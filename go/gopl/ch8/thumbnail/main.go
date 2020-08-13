22e5717aeabf8e03076efa586f1a1e40adc24ec5
gordon <1029052540@qq.com>
2020-08-14 01:45:14 +0800

    feat: add go examples

---
+++ go/gopl/ch8/thumbnail/main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sync"
+)
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		thumb, err := ImageFile(input.Text())
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		fmt.Println(thumb)
+	}
+	if err := input.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func makeThumbnails(filenames []string) {
+	for _, f := range filenames {
+		if _, err := ImageFile(f); err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func makeThumbnails2(filenames []string) {
+	for _, f := range filenames {
+		go ImageFile(f)
+	}
+}
+
+func makeThumbnails3(filenames []string) {
+	ch := make(chan struct{})
+	for _, f := range filenames {
+		go func(f string) {
+			ImageFile(f)
+			ch <- struct{}{}
+		}(f)
+	}
+	for range filenames {
+		<-ch
+	}
+}
+
+func makeThumbnails4(filenames []string) error {
+	errors := make(chan error)
+	for _, f := range filenames {
+		go func(f string) {
+			_, err := ImageFile(f)
+			errors <- err
+		}(f)
+	}
+
+	for range filenames {
+		if err := <-errors; err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func makeThumbnails5(filenames []string) (thumbfiles []string, err error) {
+	type item struct {
+		thumbFile string
+		err       error
+	}
+
+	ch := make(chan item, len(filenames))
+	for _, f := range filenames {
+		go func(f string) {
+			var it item
+			it.thumbFile, it.err = ImageFile(f)
+			ch <- it
+		}(f)
+	}
+
+	for range filenames {
+		it := <-ch
+		if it.err != nil {
+			return nil, it.err
+		}
+		thumbfiles = append(thumbfiles, it.thumbFile)
+	}
+
+	return thumbfiles, nil
+}
+
+func makeThumbnails6(filenames <-chan string) int64 {
+	sizes := make(chan int64)
+	var wg sync.WaitGroup // number of working goroutines
+	for f := range filenames {
+		wg.Add(1)
+		// worker
+		go func(f string) {
+			defer wg.Done()
+			thumb, err := ImageFile(f)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			info, _ := os.Stat(thumb) // OK to ignore error
+			sizes <- info.Size()
+		}(f)
+	}
+
+	// closer
+	go func() {
+		wg.Wait()
+		close(sizes)
+	}()
+
+	var total int64
+	for size := range sizes {
+		total += size
+	}
+	return total
+}
