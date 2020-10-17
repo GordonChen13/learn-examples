9270ef35365a642e2a86ac1ff93d7846953c8a17
gordon <1029052540@qq.com>
2020-10-18 02:33:14 +0800

    feat: add blog service router

---
+++ go/blog-service/internal/routers/router.go
@@ -0,0 +1,31 @@
+package routers
+
+import (
+	v1 "github.com/GordonChen13/learn-examples/go/blog-service/internal/routers/api/v1"
+	"github.com/gin-gonic/gin"
+)
+
+func NewRouter() *gin.Engine {
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	tag := v1.NewTag()
+	article := v1.NewArticle()
+	apiV1 := r.Group("/api/v1")
+	{
+		apiV1.POST("/tags", tag.Get)
+		apiV1.DELETE("/tags/:id", tag.Delete)
+		apiV1.PUT("/tags/:id", tag.Update)
+		apiV1.PATCH("/tags/:id/state", tag.Update)
+		apiV1.GET("/tags", tag.List)
+
+		apiV1.POST("/articles", article.Create)
+		apiV1.DELETE("/articles/:id", article.Delete)
+		apiV1.PUT("/articles/:id", article.Update)
+		apiV1.PATCH("/articles/:id/state", article.Update)
+		apiV1.GET("/articles", article.List)
+	}
+
+	return r
+}
--- go/blog-service/main.go
+++ go/blog-service/main.go
@@ -1,11 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/GordonChen13/learn-examples/go/blog-service/internal/routers"
+	"net/http"
+	"time"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "pong"})
-	})
-	r.Run()
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr: ":8080",
+		Handler: router,
+		ReadTimeout: 10*time.Second,
+		WriteTimeout: 10*time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
 }
