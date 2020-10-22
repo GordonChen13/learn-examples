c3c5b06c02505f9f0cf8caabd48df330092e07b4
gordon <1029052540@qq.com>
2020-10-22 23:59:17 +0800

    feat: go blog service add util func

---
+++ go/blog-service/pkg/app/app.go
@@ -0,0 +1,17 @@
+package app
+
+import "github.com/gin-gonic/gin"
+
+type Response struct {
+	Ctx *gin.Context
+}
+
+type Pager struct {
+	Page      int `json:"page"`
+	PageSize  int `json:"page_size"`
+	TotalRows int `json:"total_rows"`
+}
+
+func NewResponse(ctx *gin.Context) *Response {
+	return &Response{Ctx: ctx}
+}
